c0bcc155b108b9347282a42c5f1f73bdf762542d
Ashwin Raj Singh <f20202163@hyderabad.bits-pilani.ac.in>
2021-03-29 15:54:23 +0530

    some changes

--- cpp/Arrays/firstRepeatingElement.cpp
+++ cpp/Arrays/firstRepeatingElement.cpp
@@ -2,31 +2,28 @@
 #include<climits>
 using namespace std;
 
-int firstRepeat(int arr[], int n)   {
-    const int N = 10e7+5;
+int firstRepeating(int array[], int n)    {
+    int N=10e6+2;
 
     int idx[N];
     for(int i=0; i<N; i++)  {
-        idx[i]=-1;
+        idx[i] = -1;
     }
-    
+
     int minidx = INT_MAX;
-
     for(int i=0; i<n; i++)  {
-
-        if(idx[arr[i] != -1])   {
-            minidx = min(minidx , idx[arr[i]]);
+        if(idx[array[i]] != -1) {
+            minidx = min(minidx , idx[array[i]]);
         }   else    {
-            idx[arr[i]] = i;
+            idx[array[i]] = i;
         }
     }
 
-    if(minidx == INT_MAX)   {
+    if(minidx = INT_MAX)    {
         return -1;
     }   else    {
-        return minidx+1;
+        return minidx + 1;
     }
-    
 }
 
 int main()  {
@@ -34,11 +31,11 @@ int main()  {
     int n;
     cin>>n;
 
-    int arr[n];
+    int array[n];
 
-    for(int j=0; j<n; j++)  {
-        cin>>arr[j];
+    for(int i=0; i<n; i++)  {
+        cin>>array[i];
     }
-    
-    firstRepeat(arr,n);
+
+    cout<<firstRepeating(array,n);
 }
