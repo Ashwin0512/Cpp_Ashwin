e02c2b378654dec91087201e1cd6a5398fbf0c0b
Ashwin0512 <f20202163@hyderbad.bits-pilani.ac.in>
2021-04-24 16:39:59 +0530

    convert string to lower or upper case

--- cpp/strings/sameCase.cpp
+++ cpp/strings/sameCase.cpp
@@ -3,32 +3,26 @@ using namespace std;
 
 int main()  {
     string str;
-    getline(cin, str);
+    cin>>str;
+    cout<<"To convert into lower case, enter 1\nTo convert into upper case, enter 2 : ";
+    int input;
+    cin>>input;
 
-    // CONVERT TO CAPITAL 
-
-    for(int i=0; i<str.size(); i++)    {
-        if(str[i] >= 'a' && str[i] <= 'z')  {
-            str[i] -= 32;
+    if(input == 1)  {
+        for(int i=0 ; i<str.length(); i++)  {
+            if(str[i]>='A' && str[i]<='Z')  {
+                str[i] += 32;
+            }
         }
-    }
-    cout<<str<<endl;
-
-    // CONVERT TO SMALL 
-
-    for(int i=0; i<str.length(); i++)   {
-        if(str[i] >= 'A' && str[i] <= 'Z')  {
-            str[i] += 32;
+    }   else if(input ==2)  {
+        for(int i=0; i<str.length() ; i++)  {
+            if(str[i]>'a' && str[i]<'z')    {
+                str[i] -= 32;
+            }
         }
+    }   else    {
+        cout<<"INVALID INPUT";
     }
+
     cout<<str<<endl;
-
-    // There is an inbuilt function to do this as well.
-
-    string s = "nsgkjrngkjscnASFbfsnj";
-    transform(s.begin(), s.end(), s.begin(), ::toupper);
-    cout<<s<<endl;
-
-    transform(s.begin(), s.end(), s.begin() ,::tolower);
-    cout<<s<<endl;
 }
