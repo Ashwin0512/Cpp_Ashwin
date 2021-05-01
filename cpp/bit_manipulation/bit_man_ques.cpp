1c320646b2be7a19187c455777449338eb32b430
Ashwin Raj Singh <f20202163@hyderabad.bits-pilani.ac.in>
2021-05-01 16:35:37 +0530

    bit manipulation questions

--- cpp/bit_manipulation/bit_man_ques.cpp
+++ cpp/bit_manipulation/bit_man_ques.cpp
@@ -1,12 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isPowerof2(int n)   {
-    if((n&(n-1)) == 0)  {
-        return true;
-    }   else    {
-        return false;
+bool isPowerof2(int n)   {         // (n & n-1) and n have same binary representation except the rightmost setbit. 
+    return((n&(n-1)) == 0)  ;      
+}
+
+int ones_count (int n)  {
+    int count=0;
+    while(n != 0)   {
+        n = n & (n-1);
+        count++;
     }
+    return count;
 }
 
 int main()  {
@@ -15,4 +20,5 @@ int main()  {
     cin>>n;
 
     cout<<isPowerof2(n)<<endl;
+    cout<<ones_count(5)<<endl;
 }
