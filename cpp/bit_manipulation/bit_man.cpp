a2973ebc25a1a6c3e8e3013b605bdb208feecf2b
Ashwin Raj Singh <f20202163@hyderabad.bits-pilani.ac.in>
2021-05-22 23:07:07 +0530

    bit_man modified

--- cpp/bit_manipulation/bit_man.cpp
+++ cpp/bit_manipulation/bit_man.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int getBit(int n , int pos) {
-    return (n & (1 << pos)) != 0;
+    return (n & (1 << pos)) != 0;   
 }
 
 int setBit(int n , int pos)  {
@@ -14,18 +14,18 @@ int clearBit(int n , int pos)   {
 }
 
 int updateBit(int n , int pos , int val)    {
-    int toReturn = 0;
     if(val == 1)    {
-        toReturn = (n | (1<<pos));
+        return (n | (1<<pos));
     }   else if(val == 0)   {
-        toReturn = ((~(1<<pos) & n));
-    } 
-    return toReturn;
+        return (n & (~(1<<pos)));
+    }   else    {
+        return -1;
+    }
 }
 
 int main()  {
     cout<<getBit(5,2)<<endl;
     cout<<setBit(2,0)<<endl;
     cout<<clearBit(11,1)<<endl;
-    cout<<updateBit(11,1,0);
+    cout<<updateBit(4,0,1);
 }
