e9401297d9e76d081acc593e515a6dbd0e0c5c59
Ashwin0512 <singhashwin0512@gmail.com>
2021-09-11 16:56:23 +0530

    .

--- cpp/oop/implementingMemberMethods/Player.cpp
+++ cpp/oop/implementingMemberMethods/Player.cpp
@@ -1,36 +1,12 @@
 #include "Player.h"
 using namespace std;
 
-Player::Player() : name{"None"}, health{100}, xp{100} {
-    cout<<"No args constructor called"<<endl;
-}
-
-// Player::Player(string n) : name{n}, health{100}, xp{100} {
-//     cout<<"String args constructor called"<<endl;
-// }
-
-// Player::Player(double h) : health{h}, name{"None"}, xp{100} {
-//     cout<<"Health args constructor called"<<endl;
-// }
-
-// Player::Player(double x) : health{100}, name{"None"}, xp{x} {
-//     cout<<"xp args constructor called"<<endl;
-// }
-
-// Player::Player(string n,double h) : health{h}, name{n}, xp{100} {
-//     cout<<"Health, name args constructor called"<<endl;
-// }
-//
-// Player::Player(string n,double x) : health{100}, name{n}, xp{x} {
-//     cout<<"xp, name args constructor called"<<endl;
-// }
-
-// Player::Player(double h,double x) : health{h}, name{"None"}, xp{x} {
-//     cout<<"Health,xp args constructor called"<<endl;
-// }
-
 Player::Player(string n,double h,double x) : name{n},health{h}, xp{x} {
     cout<<"Three args constructor called"<<endl;
+}
+
+Player::Player() : Player("None",100,100) {     // DELEGATING CONSTRUCTOR
+    cout<<"No args constructor called"<<endl;
 }
 
 Player::~Player()   {
